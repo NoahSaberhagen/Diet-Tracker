ddb2850695f2a978858369040cd0fc3b7831659e
NoahSaberhagen <noahsaberhagen@gmail.com>
2022-03-26 13:37:10 -0400

    submit button sends user input data to local storage

--- script.js
+++ script.js
@@ -1,40 +1,27 @@
-const calorieInput = document.querySelector('#calories');
-const submitCalories = document.querySelector('#start');
+// USER INPUT TO LOCAL STORAGE
+// input variables
+const calorieInput = document.getElementById('calories');
+const proteinInput = document.getElementById('protein');
+const carbohydrateInput = document.getElementById('carbohydrate');
+const sugarInput = document.getElementById('sugar');
+const fatInput = document.getElementById('fat');
 
-// arrow function
-const abc = () => {
+// date variables
+const date = new Date();
+const today = [date.getFullYear(), date.getMonth() + 1, date.getDate()]
 
-}
-
-function xyz() {
-
-}
-
-const oldData = window.localStorage.getItem('test');
-console.log(typeof oldData);
-const parsedData = JSON.parse(oldData);
-console.log(typeof parsedData);
-
-const dataDisplay = document.createElement('h1')
-dataDisplay.innerHTML = `Calories: ${parsedData.calories}`
-
-localStorage.clear()
-
-document.body.appendChild(dataDisplay);
-
-// Can't reassign const function
-// abc = "abc"
-// abc();
-
-submitCalories.addEventListener('click', () => {
-    const dailyCalories = calorieInput.value;
-
-    const dailyData = {
-        calories: dailyCalories
-    }
-    // Look into Date objects
-    const today = Date.now().toString();
-
-    window.localStorage.setItem('test', JSON.stringify(dailyData));
-
-})
+// 'mealEntryNumber: currentMeal' to localStorage
+const submit = document.getElementById('submit');
+submit.addEventListener('click', () => {
+    let currentMeal = {
+        dateOfEntry: today,
+        calories: calorieInput.value,
+        protein: proteinInput.value,
+        carbohydrates: carbohydrateInput.value,
+        sugar: sugarInput.value,
+        fat: fatInput.value    
+    };
+    let mealEntryNumber = Object.keys(localStorage).length + 1;
+    window.localStorage.setItem(JSON.stringify(mealEntryNumber), JSON.stringify(currentMeal));
+    console.log(mealEntryNumber);
+});
