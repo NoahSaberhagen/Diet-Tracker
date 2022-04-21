198b1ae183a31626b8e0795211eb5e1af437a3e0
NoahSaberhagen <noahsaberhagen@gmail.com>
2022-04-20 23:04:53 -0400

    added comments and cleaned code

--- script.js
+++ script.js
@@ -1,28 +1,14 @@
-// USER INPUT TO LOCAL STORAGE
-// input variables
-const calorieInput = document.getElementById('calories');
-const proteinInput = document.getElementById('protein');
-const carbohydrateInput = document.getElementById('carbohydrate');
-const sugarInput = document.getElementById('sugar');
-const fatInput = document.getElementById('fat');
-
-// 'mealEntryNumber: currentMeal' to localStorage
-const form = document.querySelector('.form');
-form.addEventListener('submit', e => {
+const form = document.querySelector('.form');                       // selects form
+form.addEventListener('submit', e => { 
     e.preventDefault();
     const currentMeal = {}    
-    const mealTimeStamp = Date.now();
+    const mealTimeStamp = Date.now();                               // milliseconds since unix epoch
     const formElements = Array.from(e.target.elements);
-    formElements.forEach((input, idx) => {
+    formElements.forEach((input, idx) => {                          // loops thru form elements        
         if (idx !== formElements.length - 1){
             currentMeal[input.name] = Number(input.value)
         };
         input.value = idx === formElements.length - 1 ? "Submit" : "";
     });
-    window.localStorage.setItem(mealTimeStamp, JSON.stringify(currentMeal));
+    window.localStorage.setItem(mealTimeStamp, JSON.stringify(currentMeal));    // stores meals based on timestamp
 });
-
-//search meals
-// for(let i = 0; i <= Object.keys(localStorage).length; i++){
-    
-// }
