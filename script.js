df687d4be989a16b1c612d03c15bc5d9b287fc93
NoahSaberhagen <noahsaberhagen@gmail.com>
2022-04-21 11:28:21 -0400

    working on sorting meals by date to add daily averages feature

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const allMeals = [];
+const allMealsCalendar = [];
+const entryDate = {};
+
+const updateMeals = (newMeal, timestamp) => {                          
+    allMeals.unshift({...newMeal, timestamp});
+    window.localStorage.setItem(timestamp, JSON.stringify(newMeal));    // stores meals based on timestamp
+}; 
+
+const getInitialMeals = () => {
+    Object.keys(localStorage).forEach((mealID) => {
+        const entry = JSON.parse(localStorage.getItem(mealID));
+        entry.timestamp = Number(mealID);
+        allMeals.push(entry);
+    });
+    allMeals.sort((a, b) => b.timestamp - a.timestamp);
+};
+
 const form = document.querySelector('.form');                       // selects form
 form.addEventListener('submit', e => { 
     e.preventDefault();
@@ -10,5 +28,28 @@ form.addEventListener('submit', e => {
         };
         input.value = idx === formElements.length - 1 ? "Submit" : "";
     });
-    window.localStorage.setItem(mealTimeStamp, JSON.stringify(currentMeal));    // stores meals based on timestamp
+    updateMeals(currentMeal, mealTimeStamp);
+    console.log(allMeals);
 });
+
+
+getInitialMeals();
+console.log(allMeals);
+//////////////////////////////////////////////////////////////
+
+const sortMealsByDate = () => {
+    for(let i = 0; i < allMeals.length; i++){
+        const timeStampToDate = new Date(allMeals[i].timestamp);
+        entryDate.fullYear = timeStampToDate.getFullYear();
+        entryDate.month = timeStampToDate.getMonth();
+        entryDate.date = timeStampToDate.getDate();
+        
+        console.log(entryDate);
+
+    }
+};
+
+console.log(entryDate);
+sortMealsByDate();
+
+
